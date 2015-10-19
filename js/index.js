0460eafe57664f40a0ab46695005e5b0380393c1
lm222 <leah@dendritecorp.com>
2015-10-19 17:52:59 -0400

    submitting homework

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-//Implement the red light using jQuery. Don't forget to add the script tags.
+
+$(document).ready(function(){
+
+	$('#stopButton').click(switchRed);
+	$('#slowButton').click(switchYellow);
+	$('#goButton').click(switchGreen);
+	
+	function switchRed() {
+		clearLights();
+		$('#stopLight').css("background-color","red");
+	}
+	function switchYellow() {
+		clearLights();
+		$('#slowLight').css("background-color","yellow");
+	}
+	function switchGreen() {
+		clearLights();
+		$('#goLight').css("background-color","green");
+	}
+	function clearLights() {
+		$('#stopLight').css("background-color","black");
+		$('#slowLight').css("background-color","black");
+		$('#goLight').css("background-color","black");
+	}
+});
