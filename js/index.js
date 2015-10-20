0615deb5640f62b1e088f0205ec38203b25fba5e
JKwon <kwon.janet@gmail.com>
2015-10-20 15:59:34 -0400

    submission

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-//Implement the red light using jQuery. Don't forget to add the script tags.
+$(document).ready(function(){
+
+  $('#stopButton').click(switchStop);
+  $('#slowButton').click(switchSlow);
+  $('#goButton').click(switchGo);
+
+function switchStop() {
+  clearLights();
+  $('#stopLight').css('backgroundColor','red');
+}
+function switchSlow() {
+    clearLights();
+  $('#slowLight').css('backgroundColor','yellow');
+}
+function switchGo() {
+    clearLights();
+  $('#goLight').css('backgroundColor','green');
+}
+function clearLights() {
+  $('.bulb').css('backgroundColor','black')
+}
+
+
+});
