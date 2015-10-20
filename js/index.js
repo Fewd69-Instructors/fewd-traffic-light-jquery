ff1de760c108623e33fd6c392ea39ef41f379191
Victor <valemany@shutterstock.com>
2015-10-19 23:30:08 -0400

    doing the traffic light with jquery

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-//Implement the red light using jQuery. Don't forget to add the script tags.
+function clearAll() {
+  $('#stopLight, #slowLight, #goLight').removeClass().addClass('bulb');
+}
+
+$(function() {
+
+  $('#stopButton').click(function() {
+    clearAll();
+    $('#stopLight').addClass('red')
+  });
+
+  $('#slowButton').click(function() {
+    clearAll();
+    $('#slowLight').addClass('yellow')
+  });
+
+  $('#goButton').click(function() {
+    clearAll();
+    $('#goLight').addClass('green')
+  });
+  
+});
+
+
