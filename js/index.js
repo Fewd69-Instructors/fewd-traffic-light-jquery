ab5e2144074c14d17a8b77ecf69270651ef218c2
vickiblodnik <vblodnik@vidagroup.com>
2015-10-19 21:25:10 -0400

    Homework_Submission_VickiBlodnik
    Still working

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 //Implement the red light using jQuery. Don't forget to add the script tags.
+  $(document).ready(function(){
+
+
+  $('#stopButton').click(illuminateRed);
+  $('#slowButton').click(illuminateYellow);
+  $('#goButton').click(illuminateGreen);
+
+  function illuminateRed() {
+  	clearLights();
+  	$('stopLight').attr('class', 'red');
+  }
+
+    function illuminateYellow() {
+  	$('slowLight').attr('class', 'yellow');
+  }
+
+    function illuminateGreen() {
+  	$('goLight').attr('class', 'green');
+  }
+
+  function clearLights() {
+    $('stopLight').attr('class', 'black');
+    $('slowLight').attr('class', 'black');
+    $('goLight').attr('class', 'black');
+
+});
