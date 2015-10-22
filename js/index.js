9efe25d7fb46300a42b50d4b9e3917d2d2c57bd4
shanrobnyc <190472@Shannon-Robertson-3781.local>
2015-10-21 22:51:45 -0400

    Task completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 //Implement the red light using jQuery. Don't forget to add the script tags.
+$(document).ready(function(){
+
+  $("#stopButton").click(switchRed);
+  $("#slowButton").click(switchYellow);
+  $("#goButton").click(switchGreen);
+
+function clearLights() {
+ $("#stopLight").css("backgroundColor", "black");
+ $("#slowLight").css("backgroundColor", "black");
+ $("#goLight").css("backgroundColor", "black");
+}
+
+function switchRed() {
+  clearLights();
+  $("#stopLight").css("backgroundColor", "red");
+}
+
+function switchYellow() {
+  clearLights();
+  $("#slowLight").css("backgroundColor", "yellow");
+}
+
+function switchGreen() {
+  clearLights();
+  $("#goLight").css("backgroundColor", "green");
+}
+
+
+
+});
