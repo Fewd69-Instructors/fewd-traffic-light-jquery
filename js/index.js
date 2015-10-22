005927610f01f0ef843aa6bc769935af460b23cc
warpooljenni <jenwarpool@gmail.com>
2015-10-21 20:11:26 -0400

    Traffic Light submission
    homework is done!

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-//Implement the red light using jQuery. Don't forget to add the script tags.
+$(document).ready(function){
+  //Hide all answers when page is loaded
+  $("#answer1").hide();
+  $("#answer2").hide();
+  $("answer3").hide();
+
+
+});
