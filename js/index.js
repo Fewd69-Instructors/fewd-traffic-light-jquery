e7e62610f65bbdc9294066a1a3c5a62ed72b537b
Lizzi74 <lizzi_alberto@yahoo.com>
2015-10-27 14:24:17 -0400

    JQuery modifications

--- js/index.js
+++ js/index.js
@@ -1,1 +1,59 @@
 //Implement the red light using jQuery. Don't forget to add the script tags.
+
+// document.getElementById('stopButton').onclick = illuminateRed;
+// document.getElementById('slowButton').onclick = illuminateYellow;
+// document.getElementById('goButton').onclick = illuminateGreen;
+
+// function illuminateRed() {
+//   clearLights();
+//   document.getElementById('stopLight').style.backgroundColor = "red";
+// }
+
+// function illuminateYellow() {
+//   clearLights();
+//   document.getElementById('slowLight').style.backgroundColor = "yellow";
+// }
+
+// function illuminateGreen() {
+//   clearLights();
+//   document.getElementById('goLight').style.backgroundColor = "green";
+// }
+
+// function clearLights() {
+//   document.getElementById('stopLight').style.backgroundColor = "black";
+//   document.getElementById('slowLight').style.backgroundColor = "black";
+//   document.getElementById('goLight').style.backgroundColor = "black";
+// }
+
+
+$(document).ready(function(){
+
+  $('#stopButton').click(illuminateRed);
+  $('#slowButton').click(illuminateYellow);
+  $('#goButton').click(illuminateGreen);
+
+  function illuminateRed () {
+    clearLights();
+    $('#stopLight').css('backgroundColor', 'red');
+
+  }
+
+  function illuminateYellow() {
+    clearLights();
+    $('#slowLight').css('backgroundColor', 'yellow');
+
+  }
+
+  function illuminateGreen() {
+    clearLights();
+    $('#goLight').css('backgroundColor', 'green');
+
+  }
+
+  function clearLights() {
+    $('#stopLight').css('backgroundColor', 'black');
+    $('#slowLight').css('backgroundColor', 'black');
+    $('#goLight').css('backgroundColor', 'black');
+  }
+
+});
