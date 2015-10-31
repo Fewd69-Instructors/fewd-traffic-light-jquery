5fb35205b55f4af7e747069ea7e7d291bb503158
JGiles14 <user@users-Mac-179.local>
2015-10-31 13:03:28 -0400

    updated index.js with working jQuery

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-//Implement the red light using jQuery. Don't forget to add the script tags.
+$(document).ready(function(){
+
+  $('#stopButton').click(illuminateRed);
+  $('#slowButton').click(illuminateYellow);
+  $('#goButton').click(illuminateGreen);
+
+  function illuminateRed() {
+    clearLights();
+    $('#stopLight').css('backgroundColor', 'red');
+  }
+  function illuminateYellow() {
+    clearLights();
+    $('#slowLight').css('backgroundColor', 'yellow');
+  }
+
+  function illuminateGreen() {
+    console.log('green');
+    clearLights();
+    $('#goLight').css('backgroundColor', 'green');
+  }
+
+  function clearLights() {
+    
+    $('#stopLight').css('backgroundColor', 'black');
+    $('#slowLight').css('backgroundColor', 'black');
+    $('#goLight').css('backgroundColor', 'black');
+  }
+
+});
